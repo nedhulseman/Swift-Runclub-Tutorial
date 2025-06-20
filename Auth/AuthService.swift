cafaa0cbd4387bcdf5e354e54459d7ae12b27e4b
Ned Hulseman <ned.hulseman@gmail.com>
2025-06-20 10:51:28 -0400

    adding auth

---
+++ Auth/AuthService.swift
@@ -0,0 +1,33 @@
+//
+//  AuthService.swift
+//  runclub_tut
+//
+//  Created by Ned hulseman on 6/12/25.
+//
+
+import Foundation
+import Supabase
+
+struct Secrets {
+    static let supabaseURL = URL(string: "https://mrrwuzbnmnjbynumsuba.supabase.co")
+    static let supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ycnd1emJubW5qYnludW1zdWJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3NDAwMTQsImV4cCI6MjA2NTMxNjAxNH0.W88TyaoPHlhEShfXlXOA5eqv1ayPs4RWQjzbFTpE4vs"
+}
+
+@Observable
+final class AuthService {
+    static let shared = AuthService()
+    private var supabase = SupabaseClient(supabaseURL: Secrets.supabaseURL!, supabaseKey: Secrets.supabaseKey)
+    
+    var currentSession: Session?
+    private init () {}
+    
+    func magicLinkLogin(email: String) async throws {
+        try await supabase.auth.signInWithOTP(
+          email: email,
+          redirectTo: URL(string: "com.runclub-tutorial://login-callback")!
+        )
+    }
+    func handleOpenURL(url: URL) async throws {
+        currentSession = try await supabase.auth.session(from: url)
+    }
+}
---
+++ Auth/LoginView.swift
@@ -0,0 +1,51 @@
+//
+//  LoginView.swift
+//  runclub_tut
+//
+//  Created by Ned hulseman on 6/19/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State var email = ""
+    var body: some View {
+        VStack{
+            TextField("E-mail", text:$email)
+                .bold()
+                .padding()
+                .frame(maxWidth: .infinity)
+                .foregroundStyle(.black)
+                .background(.ultraThinMaterial)
+                .clipShape(Capsule())
+            
+            
+            Button {
+                Task {
+                    do {
+                        try await AuthService.shared.magicLinkLogin(email: email)
+                    } catch{
+                        print(error.localizedDescription)
+                    }
+                }
+            } label: {
+                Text("Login")
+                    .bold()
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .foregroundStyle(.black)
+                    .background(Color.yellow)
+                    .clipShape(Capsule())
+            }
+            .disabled(email.count < 7)
+        }
+        .padding()
+        .onOpenURL(perform: {url in
+            
+        })
+    }
+}
+
+#Preview {
+    LoginView()
+}
